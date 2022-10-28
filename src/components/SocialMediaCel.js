8123ebf897f93636831b968c4b3795d3befc6aac
Makita7 <sophiamakita@gmail.com>
2022-10-28 14:49:44 -0300

    corrected contact links

--- src/components/SocialMediaCel.js
+++ src/components/SocialMediaCel.js
@@ -1,13 +1,16 @@
 import {AiOutlineMail, AiOutlineFacebook, AiOutlineInstagram} from 'react-icons/ai';
 import {BsWhatsapp} from 'react-icons/bs';
 
+const FaceBook = 'https://m.facebook.com/profile.php?id=100085575399658&eav=AfYpOMpsYfmMX4uBPRZnR2sK_5IFf1Wp24Bm1p3J3KnsxYwjDEUhN7xxNgpdQXF9y9E&tsid=0.9356544011772432&source=result';
+const Insta = 'https://www.instagram.com/microcosmosapp/';
+
 function SocialMediaCel() {
     return (
         <div className='OnlyCell'>
-            <a className='icon' href='mailto:neuquina007@gmail.com'><AiOutlineMail size={20}/></a>
-            <a className='icon' href='https://facebook.com/'><AiOutlineFacebook size={20}/></a>
+            <a className='icon' href='mailto:microcosmosapp@gmail.com'><AiOutlineMail size={20}/></a>
+            <a className='icon' href={FaceBook} ><AiOutlineFacebook size={20}/></a>
             <a className='icon' href='https://wa.me/5492995289706?text='><BsWhatsapp size={20}/></a>
-            {/* <a className='icon' href=''><AiOutlineInstagram size={20}/></a> */}
+            <a className='icon' href={Insta} ><AiOutlineInstagram size={20}/></a>
         </div>
     );
 }
